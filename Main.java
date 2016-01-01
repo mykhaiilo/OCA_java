5bc0902a43aa9174620a9053829e5c6d8f4de7e0
mykhailo.dvolitka <mykhailo.dvolitka@gmail.com>
2016-01-01 19:49:07 +0200

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+package Lesson_1;
+
+/**
+ * Created by admin on 01.01.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
